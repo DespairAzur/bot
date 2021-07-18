4fffc51e8183b0046bc120241e0d4c917b8201bb
DespairAzur <87237887+DespairAzur@users.noreply.github.com>
2021-07-18 03:16:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on("ready", () => {
     console.log(`Bot online !`);
-    console.log(`${client.channels.size}`);
+    console.log(`${client.channels.cache.size}`);
 
 });
 
